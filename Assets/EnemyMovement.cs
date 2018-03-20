112b67b37e2fc4c1e7ebfabf4f75d84ba4cc2387
Иван Хомичков <ivan.khomichkov@gmail.com>
2018-03-20 21:53:53 +0300

    Added waypoints and made a script for enemy to move throw that waypoints with a certain movement speed. First look at Coroutine and Enumirators.

---
+++ Assets/EnemyMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+    [SerializeField] List<Waypoint> waypoints;
+
+    [SerializeField] float EnemyMovementSpeed=1f;
+	// Use this for initialization
+	void Start ()
+    {
+        StartCoroutine(MoveThrowWaypoints());
+      
+    }
+
+    private IEnumerator MoveThrowWaypoints()
+    {
+        foreach (var item in waypoints)
+        {
+            transform.position = item.transform.position;
+            yield return new WaitForSeconds(EnemyMovementSpeed);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
