e0cffe26e9933bda8100da43035aa4b5537e1da4
Иван Хомичков <ivan.khomichkov@gmail.com>
2018-03-20 17:44:38 +0300

    Added a little editor script that helps positioning my cubes in the scene.

---
+++ Assets/EditorHelper.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class EditorHelper : MonoBehaviour {
+    [Range(1f,20f)]
+    [SerializeField] float GridSize = 10f;
+    private void Update()
+    {
+        Vector3 NewPosition;
+
+        NewPosition.x = Mathf.RoundToInt(transform.position.x/ GridSize) * GridSize;
+        NewPosition.z = Mathf.RoundToInt(transform.position.z / GridSize) * GridSize;
+        transform.position = new Vector3(NewPosition.x,0f,NewPosition.z);
+    }
+}
